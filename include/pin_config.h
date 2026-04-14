3f7dde94496d7d2a92196a60ca58a4148c7ee09f
MEISSSTER <azaz38918@gmail.com>
2026-04-14 21:40:37 +0300

    Initida]]d

---
+++ include/pin_config.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// SPI шина
+#define SPI_CLK         GPIO_NUM_18
+#define SPI_MOSI        GPIO_NUM_23
+#define SPI_MISO        GPIO_NUM_19
+
+// ILI9488 дисплей
+#define SPI_CS          GPIO_NUM_5
+#define LCD_DC          GPIO_NUM_26
+#define LCD_RST         GPIO_NUM_4
+#define LCD_BL          GPIO_NUM_27
+
+
+#define LED_PIN     GPIO_NUM_44
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,34 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "driver/gpio.h" // Основная библиотека для работы с пинами
-#include "Stepmotor.h"
+#include "driver/gpio.h"
+#include "lvgl.h"
+#include "pin_config.h"
 
-#define LED_PIN GPIO_NUM_44  // пин, к которому подключен светодиод
+#define TFT_HOR_RES 320
+#define TFT_VER_RES 480
+
+// bool lvgl_init(){
+//     lv_display_t * disp = lv_display_create(TFT_HOR_RES, TFT_VER_RES);
+//     lv_display_set_buffers(disp, buf1, buf2, sizeof(buf1), LV_DISPLAY_RENDER_MODE_PARTIAL);
+//     lv_display_set_flush_cb(disp, flush_cb);
+
+//     lv_indev_t * indev = lv_indev_create();
+//     lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER);
+//     lv_indev_set_read_cb(indev, read_cb);
+
+//     return true;
+
+// }
+
+
 extern "C" void app_main(void) {
-    StepMotor stepMotor;
-    stepMotor.begin();
-    stepMotor.enable();
-  
 
-    while(1) {
-        stepMotor.move(200, DIR_UP);  // Двигаем вверх на 200 шагов
-        stepMotor.move(100, DIR_DOWN); // Двигаем вниз на 100 шагов
-        // 1. Сброс и настройка пина
-        gpio_reset_pin(LED_PIN);
-        gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+    lv_init();
+
+    while(1){
+        lv_timer_handler();
+        vTaskDelay(1);
+
     }
 }
